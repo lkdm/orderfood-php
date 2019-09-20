cf34a7d5500845e7ae070efd59d2a71c8a3bf406
Luke Martin <github@ldmartin.com>
2019-09-20 15:12:37 +1000

    Added untested edit method to DishController.lphp

--- app/Http/Controllers/DishController.php
+++ app/Http/Controllers/DishController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Dish;
-use App\User;
 use Auth;
 
 class DishController extends Controller
@@ -60,6 +59,8 @@ class DishController extends Controller
          *  Accepts a maximum of 2MB for photo
          */
 
+        $restaurant_id = Auth::user()->id;
+
         // Guard: Only restaurants can create new dishes.
         if (Auth::user()->role != "restaurant") {
             return redirect('/');
@@ -67,21 +68,30 @@ class DishController extends Controller
 
         // Guards: Input validation
         $this->validate($request, [
-            'name'=>'required|max:255',
+            'name'=>'required|min:8|max:255',
             'price'=>'required|numeric|digits_between:0,9999',
             'photo'=>'required|mimes:jpeg,png,jpg,gif,svg|max:2048'
         ]);
 
+        // Guard: Check for unique name
+        $similar = Dish::whereRaw('Name LIKE ? AND restaurant_id = ?', array($request->name, $restaurant_id))->count();
+        if ($similar != 0) {
+            $error = \Illuminate\Validation\ValidationException::withMessages([
+                'name' => ['You already have a dish with the same name in your restaurant.']
+             ]);
+             throw $error;
+        }
+
+        // Create a new dish and map its details
         $dish = new Dish();
         $dish->name = $request->name;
         $dish->price = $request->price;
         $dish->photo = $request->photo;
         $dish->approved = 0;
-        // TODO: Check for unique name
-        $dish->restaurant_id = Auth::user()->id;
+        $dish->restaurant_id = $restaurant_id;
+
         $dish->save();
         return redirect("dish/$dish->id");
-
     }
 
     /**
@@ -104,7 +114,12 @@ class DishController extends Controller
      */
     public function edit($id)
     {
-        //
+        // Guard: Only restaurants can edit dishes.
+        if (Auth::user()->role != "restaurant") {
+            return redirect('/');
+        }
+
+        return view('dishes.edit');
     }
 
     /**
@@ -116,7 +131,44 @@ class DishController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $restaurant_id = Auth::user()->id;
+
+        // Guard: Only restaurants can update dishes.
+        if (Auth::user()->role != "restaurant") {
+            return redirect('/');
+        }
+
+        // Guards: Input validation
+        $this->validate($request, [
+            'name'=>'required|min:8|max:255',
+            'price'=>'required|numeric|digits_between:0,9999',
+            'photo'=>'required|mimes:jpeg,png,jpg,gif,svg|max:2048'
+        ]);
+
+        // Guard: Check for unique name
+        $similar = Dish::whereRaw('Name LIKE ? AND restaurant_id = ?', array($request->name, $restaurant_id))->count();
+        if ($similar != 0) {
+            $error = \Illuminate\Validation\ValidationException::withMessages([
+                'name' => ['You already have a dish with the same name in your restaurant.']
+             ]);
+             throw $error;
+        }
+
+        // Update the existing dish and map its details
+        $dish = Dish::findOrFail($id);
+        $dish->name = $request->name;
+        $dish->price = $request->price;
+        $dish->photo = $request->photo;
+
+        // For the sake of development, updates are automatically approved.
+        // TODO: Upon production release, change this to 0.
+        $dish->approved = 1;
+
+        // Updating the restaurant_ID should never be necessary.
+        // $dish->restaurant_id = $restaurant_id;
+
+        $dish->save();
+        return redirect("dish/$dish->id");
     }
 
     /**
@@ -127,6 +179,29 @@ class DishController extends Controller
      */
     public function destroy($id)
     {
-        //
+        // Guard: Only restaurants can destroy dishes.
+        if (Auth::user()->role != "restaurant") {
+            return redirect('/');
+        }
+
+        $dish = Dish::findOrFail($id);
+        $dish->delete();
+        return redirect("dish");
+    }
+
+    /**
+     * Admin-only: Approve the dish
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function approve($id)
+    {
+        // Guard: Only admins can approve dishes.
+        if (Auth::user()->role != "administrator") {
+            return redirect('/');
+        }
+        // Check that dish exists
+        // Approve the dish
     }
 }
